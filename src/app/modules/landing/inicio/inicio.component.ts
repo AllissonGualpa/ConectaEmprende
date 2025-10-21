f11753255154ea166dd9855ef665123cb1803f60
melanie <melanieleon20@hotmail.com>
2025-10-21 13:15:43 -0500

    fix filtros del component search bar en evento, emprendimiento

--- src/app/modules/landing/inicio/inicio.component.ts
+++ src/app/modules/landing/inicio/inicio.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from '../../../layout/navbar/navbar.component';
 import { FooterComponent } from '../../../layout/footer/footer.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent,MatButtonModule,
+  imports: [CommonModule, RouterLink,NavbarComponent, FooterComponent,MatButtonModule,
     MatIconModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
