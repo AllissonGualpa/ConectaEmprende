02d48800ce11768f409d084f2c898b2c96f9dbd7
melanie leon <melanieleon20@hotmail.com>
2025-12-10 14:00:11 -0500

    mejora: agregar resaltado activo en el menú de navegación del admin

--- src/app/layout/navbar-admin/navbar-admin.component.ts
+++ src/app/layout/navbar-admin/navbar-admin.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../modules/auth/auth.service';
 
 @Component({
   selector: 'app-navbar-admin',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar-admin.component.html',
   styleUrls: ['./navbar-admin.component.css']
 })
